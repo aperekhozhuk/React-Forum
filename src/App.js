663edd7a5d8f97eb50cfd432d2bfab45298263ff
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2020-01-30 18:34:03 +0200

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import PostPage from './screens/PostPage/PostPage';
 import NewPostPage from './screens/NewPostPage/NewPostPage';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Router>
         <Header />
-        <Router>
-          <Switch>
-            <Route exact path="/" component={MainPage}></Route>
-            <Route exact path="/posts/new" component={NewPostPage}></Route>
-            <Route path="/posts/:id([1-9][0-9]*)" component={PostPage}></Route>
-            <Redirect to="/" />
-          </Switch>
-        </Router>
-      </React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" render={ (props) => (
+            <MainPage />
+          )}>
+          </Route>
+
+          <Route exact path="/posts/new"render={ (props) => (
+            <NewPostPage />
+          )}>
+          </Route>
+
+          <Route path="/posts/:id([1-9][0-9]*)" render={ (props) => (
+            <PostPage {...props}/>
+          )}>
+          </Route>
+
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div style={{border: '5px solid red', marginBottom: '20px'}}>
+          <Link to="/">Main Page</Link>
+          <Link to="/posts/new">Create new post</Link>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
