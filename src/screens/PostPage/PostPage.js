6ae0e338bc234b12306d73b6d77514e18d9ea828
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2020-01-30 21:05:14 +0200

    Fill PostPage screen

--- src/screens/PostPage/PostPage.js
+++ src/screens/PostPage/PostPage.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class PostPage extends Component {
+  state = {
+    post: {}
+  }
+
+  componentDidMount() {
+    axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${this.props.match.params.id}`
+    ).then(res => this.setState({ post: res.data }))
+  }
+
   render() {
     return (
       <div>
-        Post {this.props.match.params.id}
+        <h3>Posted by user with id: {this.state.post.userId}</h3>
+        <hr></hr>
+        <h1>{this.state.post.title}</h1>
+        <hr></hr>
+        <p>{this.state.post.body}</p>
       </div>
     )
   }
