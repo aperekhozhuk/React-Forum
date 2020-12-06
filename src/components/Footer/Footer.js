4dc60ea5d3121a6dcbf9e5e416ae26b5895e08e8
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2020-12-07 01:05:48 +0200

    Add links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,29 @@ class Footer extends Component {
   render() {
     return (
       <footer className="jumbotron mb-0 p-3 bg-muted">
-        <h1 className="text-info">Footer!</h1>
+        <h3 className="text-info">Runs with (links to GitHub):</h3>
+        <ul>
+          <li>
+            <a
+              href = "https://github.com/aperekhozhuk/FlaskAPI"
+              className = "text-decoration-none ml-3 pd-2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BackEnd
+            </a>
+          </li>
+          <li>
+            <a
+              href = "https://github.com/aperekhozhuk/React-Forum"
+              className = "text-decoration-none ml-3 pd-2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              FrontEnd
+          </a>
+          </li>
+        </ul>
       </footer>
     )
   }
