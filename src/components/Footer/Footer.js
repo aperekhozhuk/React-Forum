9e20d26b09f2a75ad97de6b864144d8b4d3daad1
aperekhozhuk <37818078+aperekhozhuk@users.noreply.github.com>
2020-12-07 00:04:30 +0200

    Add github links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,19 @@ class Footer extends Component {
   render() {
     return (
       <footer className="jumbotron mb-0 p-3 bg-muted">
-        <h1 className="text-info">Footer!</h1>
+        <h1 className="text-info">
+          <h2>Links:</h2>
+            <p>
+              <a href = "https://github.com/aperekhozhuk/FlaskAPI" target="_blank">
+                Back end (GitHub)
+              </a>
+            </p>
+            <p>
+              <a href = "https://github.com/aperekhozhuk/React-Forum" target="_blank">
+                Front End (GitHub)
+              </a>
+            </p>
+        </h1>
       </footer>
     )
   }
